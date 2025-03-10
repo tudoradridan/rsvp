40834a22d706819804ac834667f4f10c83c62053
Andrei Buhus <andrei.buhus.ext@siemens.com>
2025-03-10 16:20:20 +0200

    middleware commit 3

--- middleware.ts
+++ middleware.ts
@@ -1,6 +1,5 @@
 import { type NextRequest } from "next/server";
 import { NextResponse } from "next/server";
-
 import { updateSession } from "@/app/utils/supabase/middleware";
 
 export async function middleware(request: NextRequest) {
@@ -12,3 +11,16 @@ export async function middleware(request: NextRequest) {
     return new NextResponse("Internal Server Error", { status: 500 });
   }
 }
+
+export const config = {
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * Feel free to modify this pattern to include more paths.
+     */
+    "/((?!_next/static|_next/image|favicon.ico|.*\\.(?:svg|png|jpg|jpeg|gif|webp)$).*)",
+  ],
+};
