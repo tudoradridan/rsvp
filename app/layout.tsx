fd6d305d92201d24653c32aa0381484720eb3b77
Adridan Tudor <tudor.adridan@stsnet.ro>
2025-07-07 15:54:16 +0300

    Change language

--- app/layout.tsx
+++ app/layout.tsx
@@ -38,7 +38,7 @@ export default function RootLayout({
   }, []);
 
   return (
-    <html lang="en">
+    <html lang="ro">
       <Head>
         <link rel="icon" href="/favicon.ico" />
       </Head>
